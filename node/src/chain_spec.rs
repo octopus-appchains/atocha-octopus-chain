318bee37b364827d1efcfca3a3225f8b6fbcf5a4
linhai <630086711@qq.com>
2022-02-07 11:16:19 +0800

    Update members account .

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -112,16 +112,12 @@ pub fn development_config() -> Result<ChainSpec, String> {
 					get_account_id_from_seed::<sr25519::Public>("Dave"),
 					get_account_id_from_seed::<sr25519::Public>("Eve"),
 					get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+				]),
+				vec![
 					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
 					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
 					get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
 					get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
-				]),
-				vec![
-					get_account_id_from_seed::<sr25519::Public>("Alice"),
-					get_account_id_from_seed::<sr25519::Public>("Bob"),
-					get_account_id_from_seed::<sr25519::Public>("Charlie"),
-					get_account_id_from_seed::<sr25519::Public>("Dave"),
 				],
 				true,
 			)
